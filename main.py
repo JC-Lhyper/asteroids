da1dd6ca0482ce01c224638f92561fe2dfaa1b6a
JC-Lhyper <lippert.jc@gmail.com>
2024-09-23 12:30:35 +0200

    ajout des collisions qui provoquent la fin du jeu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -14,14 +15,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    field = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroidfield = AsteroidField()
+    asteroid_field = AsteroidField()
     
 
     while True:
@@ -29,9 +29,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        screen.fill("black")
+
         updatable.update(dt)
 
-        screen.fill("black")
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game over!")
+                sys.exit()
 
         for obj in drawable:
             obj.draw(screen)
